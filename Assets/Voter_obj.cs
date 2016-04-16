acc9253402b7f7ed2bd3d6978069d62c464804d0
Eric <evo>
2016-04-16 21:00:14 +0200

    cs new

--- Assets/Voter_obj.cs
+++ Assets/Voter_obj.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class Voter_obj {
 	
 	public GameObject gameobj;
+	private BoxCollider2D box;
 
 	public Voter_obj (float x, float y) {
 		gameobj = new GameObject ();
 		gameobj.name = "Sprite (" + x + "," + y + ")";
 		gameobj.transform.position = new Vector3 (x, y, 0);
 		gameobj.AddComponent<SpriteRenderer> ();
+		gameobj.AddComponent<BoxCollider2D> ();
 		gameobj.AddComponent<GetMoBuUp> ();
+	}
+
+
+	public void Color() {
+
+
 	}
 
 }
