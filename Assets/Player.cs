2a602f5c09a88fbedb868cc52398d4999f742f37
Eric <evo>
2016-04-16 22:36:16 +0200

    new2

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Color c_chosen;
+	public Button but;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void setActiveColor() {
+		GameObject.Find ("active").GetComponent<Text> ().color = new Color(1,1,0);
+	}
+
+
+}
