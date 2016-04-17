ad1543ad7d3b5bac467b8af10650dec052e0e186
Marcus Hoffmann <bubuiic@aol.com>
2016-04-17 13:30:06 +0200

    colors

--- Assets/GameConfig.cs
+++ Assets/GameConfig.cs
@@ -17,11 +17,13 @@ public class GameConfig
 
 	public int numberOfDistricts = 5;
 	public List<Color> colorList = new List<Color> (new Color [] {
-		new Color (0, 0, 1),
-		new Color (0, 1, 0),
-		new Color (0, 1, 1),
-		new Color (1, 0, 0),
-		new Color (1, 0, 1)
+		new Color (0.886f,0.29f,0.349f),
+		new Color (0.918f,0.745f,0.302f),
+		new Color (0.263f,0.306f,0.631f),
+		new Color (0.341f,0.765f,0.251f),
+		new Color (0.918f,0.616f,0.302f),
+		new Color (0.431f,0.235f,0.62f),
+		new Color (0.204f,0.482f,0.573f)
 	});
 }
 
