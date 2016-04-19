88f13fcd1cc674410cee1834da1209d2cc16044f
Marcus Hoffmann <bubuiic@aol.com>
2016-04-19 15:11:08 +0200

    win screen

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player {
 
@@ -7,9 +8,11 @@ public class Player {
 	public Sprite sprite;
 	public bool isHuman;
 	public AI ai;
+	public HashSet<District> districtSet;
 
 	public Player(int index) {
 		this.index = index;
 		this.isHuman = true;
+		this.districtSet = new HashSet<District>();
 	}
 }
